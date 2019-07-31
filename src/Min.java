281d2806ae3f23bb0e86a533dae3f55f725c154e
sakchai srimakorn <sakchaisrim@gmail.com>
2019-07-31 11:33:15 +0700

    Initial commit

---
+++ src/Min.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Min {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int min;
+
+        System.out.println("Enter num1:");
+        int n1 =scan.nextInt();
+        min = n1;
+
+        System.out.println("Enter num2");
+        int n2 =scan.nextInt();
+        if(n2<min){
+            min = n2;
+        }
+
+        System.out.println("enter num3:");
+        int n3 =scan.nextInt();
+        if(n3<min){
+            min = n3;
+        }
+        System.out.println("Minimul number is "+min);
+        scan.close();
+    }
+}
